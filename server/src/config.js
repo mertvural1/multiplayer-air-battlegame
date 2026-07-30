923acaa192a31b1e0be37ed6ac94a83d9b0a6a3f
mertvural1 <mertvural90@gmail.com>
2026-07-30 18:38:13 +0300

    chore: config edit

--- server/src/config.js
+++ server/src/config.js
@@ -1,4 +1,4 @@
 export const config = Object.freeze({
   port: Number(process.env.PORT) || 3001,
-  clientOrigin: process.env.CLIENT_ORIGIN || 'http://localhost:5173'
+  clientOrigin: "https://multiplayer-air-battlegame-client.vercel.app/"
 });
