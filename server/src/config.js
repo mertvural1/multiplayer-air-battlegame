ca0bd2b546b80968afc1b91f926461eb933cf28a
mertvural1 <mertvural90@gmail.com>
2026-07-30 18:45:38 +0300

    test

--- server/src/config.js
+++ server/src/config.js
@@ -1,4 +1,4 @@
 export const config = Object.freeze({
   port: Number(process.env.PORT) || 3001,
-  clientOrigin: "https://multiplayer-air-battlegame-client.vercel.app/"
+  clientOrigin: "https://multiplayer-air-battlegame-2.onrender.com"
 });
